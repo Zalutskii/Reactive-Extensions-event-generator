99415d65fabbdd978788db7eb5d72cfbb5286d1e
Ivan Zalutskii <MozGangster@narod.ru>
2021-01-13 01:23:28 +0300

    RxGenerator refactoring

--- SourceGenerator/RxGenerator.cs
+++ SourceGenerator/RxGenerator.cs
@@ -11,8 +11,8 @@ namespace RxSourceGenerator
     [Generator]
     public class RxGenerator : ISourceGenerator
     {
-        const string startText = "using System;using System.Reactive.Linq;namespace RxMethodGenerator{public static class RxGeneratedMethods{}}";
-        private static readonly SymbolDisplayFormat SymbolDisplayFormat = new SymbolDisplayFormat(
+        const string StartText = "using System;using System.Reactive.Linq;namespace RxMethodGenerator{public static class RxGeneratedMethods{}}";
+        private static readonly SymbolDisplayFormat SymbolDisplayFormat = new(
             typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
             genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters |
                              SymbolDisplayGenericsOptions.IncludeTypeConstraints |
@@ -28,22 +28,23 @@ namespace RxSourceGenerator
 
         public void Execute(GeneratorExecutionContext context)
         {
-            if (!(context.SyntaxReceiver is SyntaxReceiver receiver)) return;
+            if (context.SyntaxReceiver is not SyntaxReceiver receiver) return;
 
-            if (!(receiver.GenerateCandidates.Any()))
+            if (!receiver.GenerateCandidates.Any())
             {
-                context.AddSource("RxGenerator.cs", startText);
+                context.AddSource("RxGenerator.cs", StartText);
                 return;
             }
 
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new();
             sb.AppendLine("using System;");
             sb.AppendLine("using System.Reactive.Linq;");
             sb.AppendLine("namespace RxMethodGenerator{");
             sb.AppendLine("    public static class RxGeneratedMethods{");
 
-            foreach ((string classType, string eventName, string eventType, List<string> argumentTypes, bool isStub) in GetExtensionMethodInfo(context,
-                receiver))
+            foreach ((string classType, string eventName, string eventType, List<string> argumentTypes) in
+                GetExtensionMethodInfo(context,
+                    receiver))
             {
                 string tupleTypeStr;
                 string conversionStr;
@@ -61,39 +62,33 @@ namespace RxSourceGenerator
                     default:
                         tupleTypeStr =
                             $"({string.Join(", ", argumentTypes.Select((x, i) => $"{x} Item{i + 1}{x.Split('.').Last()}"))})";
-                        string objStr = string.Join(", ", argumentTypes.Select((x, i) => $"obj{i}"));
+                        string objStr = string.Join(", ", argumentTypes.Select((_, i) => $"obj{i}"));
                         conversionStr = $"conversion => ({objStr}) => conversion(({objStr})),";
                         break;
                 }
 
-                sb.AppendLine(
-                    @$"        public static IObservable<{tupleTypeStr}> Rx{eventName}(this {classType} obj)");
-                sb.AppendLine(@"        {");
-                if (isStub)
-                {
-                    sb.AppendLine("            throw new Exception('RxGenerator stub');");
-                }
-                else
-                {
-                    sb.AppendLine("            if (obj == null) throw new ArgumentNullException(nameof(obj));");
-                    sb.AppendLine(@$"            return Observable.FromEvent<{eventType}, {tupleTypeStr}>(");
-                    sb.AppendLine(@$"            {conversionStr}");
-                    sb.AppendLine(@$"            h => obj.{eventName} += h,");
-                    sb.AppendLine(@$"            h => obj.{eventName} -= h);");
-                }
-                sb.AppendLine("        }");
+                sb.AppendLine(@$"        public static IObservable<{tupleTypeStr}> Rx{eventName}(this {classType} obj)");
+                sb.AppendLine( @"        {");
+
+                sb.AppendLine(  "            if (obj == null) throw new ArgumentNullException(nameof(obj));");
+                sb.AppendLine(@$"            return Observable.FromEvent<{eventType}, {tupleTypeStr}>(");
+                sb.AppendLine(@$"            {conversionStr}");
+                sb.AppendLine(@$"            h => obj.{eventName} += h,");
+                sb.AppendLine(@$"            h => obj.{eventName} -= h);");
+
+                sb.AppendLine(  "        }");
             }
-            sb.AppendLine("    }");
-            sb.AppendLine("}");
+            sb.AppendLine(      "    }");
+            sb.AppendLine(      "}");
 
             context.AddSource("RxGenerator.cs", sb.ToString());
         }
 
-        private static IEnumerable<(string ClassType, string EventName, string EventType, List<string> ArgumentTypes, bool IsStub)>
+        private static IEnumerable<(string ClassType, string EventName, string EventType, List<string> ArgumentTypes)>
             GetExtensionMethodInfo(GeneratorExecutionContext context, SyntaxReceiver receiver)
         {
             HashSet<(string ClassType, string EventName)>
-                hashSet = new HashSet<(string ClassType, string EventName)>();
+                hashSet = new();
             foreach (MemberAccessExpressionSyntax syntax in receiver.GenerateCandidates)
             {
                 SemanticModel model = context.Compilation.GetSemanticModel(syntax.SyntaxTree);
@@ -109,24 +104,24 @@ namespace RxSourceGenerator
                 if (typeSymbol == null) continue;
                 string eventName = syntax.Name.ToString().Substring(2);
 
-                if (!(typeSymbol.GetMembersOfType<IEventSymbol>().FirstOrDefault(m => m.Name == eventName) is { } ev)
-                ) continue;
+                if (typeSymbol.GetMembersOfType<IEventSymbol>().FirstOrDefault(m => m.Name == eventName) is not { } ev)
+                 continue;
 
-                if (!(ev.Type is INamedTypeSymbol namedTypeSymbol)) continue;
+                if (ev.Type is not INamedTypeSymbol namedTypeSymbol) continue;
                 if (namedTypeSymbol.DelegateInvokeMethod == null) continue;
                 if (!hashSet.Add((typeSymbol.ToString(), ev.Name))) continue;
 
                 string fullType = namedTypeSymbol.ToDisplayString(SymbolDisplayFormat);
                 List<string> typeArguments = namedTypeSymbol.DelegateInvokeMethod.Parameters
                     .Select(m => m.Type.ToDisplayString(SymbolDisplayFormat)).ToList();
-                yield return (typeSymbol.ToString(), ev.Name, fullType, typeArguments, false);
+                yield return (typeSymbol.ToString(), ev.Name, fullType, typeArguments);
             }
         }
 
         private class SyntaxReceiver : ISyntaxReceiver
         {
             public List<MemberAccessExpressionSyntax> GenerateCandidates { get; } =
-                new List<MemberAccessExpressionSyntax>();
+                new();
 
             public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
             {
@@ -134,7 +129,6 @@ namespace RxSourceGenerator
                 if (syntax.HasTrailingTrivia || syntax.Name.IsMissing) return;
                 if (!syntax.Name.ToString().StartsWith("Rx")) return;
                 GenerateCandidates.Add(syntax);
-
             }
         }
     }
