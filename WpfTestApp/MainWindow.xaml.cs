c2a44e0776d95e5519fb644718628688dd9072b7
Ivan Zalutskii <MozGangster@narod.ru>
2021-01-13 01:24:08 +0300

    Added WPF example

---
+++ WpfTestApp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Reactive.Linq;
+using RxMethodGenerator;
+
+namespace WpfTestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.RxMouseMove().Subscribe(e=> 
+            {
+                Label1.Content = e.Item2MouseEventArgs.GetPosition(null);
+            });
+        }
+    }
+}
